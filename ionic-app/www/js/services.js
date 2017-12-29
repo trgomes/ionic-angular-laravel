279f9a4a59fb1c16429f433ebc7bda350c396214
trgomes <trtg10@gmail.com>
2017-12-29 21:54:30 -0200

    add requisição no serviço

--- ionic-app/www/js/services.js
+++ ionic-app/www/js/services.js
@@ -1,17 +1,25 @@
 angular.module('service', ['ngStorage'])
 
-    .service('webService', function ($localStorage) {
+    .service('webService', function ($localStorage, $http) {
 
         var _getOfertas = function () {
 
+            $http
+                .get('http://localhost:8000/api/ofertas')
+                .then(function(retorno){                    
+                    $localStorage.ofertas = retorno.data;
+                }, function(erro){
+
+                });
+
             //Verifica se existe algo cadastrado no localStorage do navegador
-            if (!$localStorage.ofertas) {
+            /* if (!$localStorage.ofertas) {
                 $localStorage.ofertas = [
                     { id: '1', titulo: 'Café', descricao: 'Descrição', valor: 4.45, valor_f: 'R$ 4,45', validade: '2017-12-26', img: '' },
                     { id: '2', titulo: 'Manteiga', descricao: 'Descrição', valor: 4.45, valor_f: 'R$ 4,45', validade: '2017-12-26', img: '' },
                     { id: '3', titulo: 'Pão', descricao: 'Descrição', valor: 4.45, valor_f: 'R$ 4,45', validade: '2017-12-26', img: '' }
                 ];
-            }
+            } */
 
             return $localStorage.ofertas;
 
